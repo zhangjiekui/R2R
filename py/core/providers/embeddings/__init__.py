5b2cd40a199d10c64ccc8180772b3b9f209788bf
zhangjiekui <33198334+zhangjiekui@users.noreply.github.com>
2025-08-29 03:05:34 +0800

    Update __init__.py
    export XinferenceEmbeddingProvider

--- py/core/providers/embeddings/__init__.py
+++ py/core/providers/embeddings/__init__.py
@@ -1,9 +1,11 @@
 from .litellm import LiteLLMEmbeddingProvider
 from .ollama import OllamaEmbeddingProvider
 from .openai import OpenAIEmbeddingProvider
+from .xinference import XinferenceEmbeddingProvider
 
 __all__ = [
     "LiteLLMEmbeddingProvider",
     "OpenAIEmbeddingProvider",
     "OllamaEmbeddingProvider",
+    "XinferenceEmbeddingProvider",
 ]
