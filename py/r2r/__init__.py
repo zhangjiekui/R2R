23a5f6cf9d22af338f3504cde226e7326cdbc23d
zhangjiekui <zhangjiekui@gmail.com>
2025-09-15 17:40:14 +0800

    配置HATCHET_CLIENT环境变量，可在Docker外正常运行r2r serve并启用provider = "hatchet"

--- py/r2r/__init__.py
+++ py/r2r/__init__.py
@@ -23,6 +23,9 @@ from shared import __all__ as shared_all
 
 # HATCHET_CLIENT_TOKEN可以在Hatchet-Dashboard界面的Settings面板中获取 https://docs.hatchet.run/home/setup
 os.environ['HATCHET_CLIENT_TOKEN']="eyJhbGciOiJFUzI1NiIsImtpZCI6InQ3ZF81ZyJ9.eyJhdWQiOiJodHRwOi8vaG9zdC5kb2NrZXIuaW50ZXJuYWw6NzI3NCIsImV4cCI6NDkxMTUwMTQzOSwiZ3JwY19icm9hZGNhc3RfYWRkcmVzcyI6ImhhdGNoZXQtZW5naW5lOjcwNzciLCJpYXQiOjE3NTc5MDE0MzksImlzcyI6Imh0dHA6Ly9ob3N0LmRvY2tlci5pbnRlcm5hbDo3Mjc0Iiwic2VydmVyX3VybCI6Imh0dHA6Ly9ob3N0LmRvY2tlci5pbnRlcm5hbDo3Mjc0Iiwic3ViIjoiNzA3ZDA4NTUtODBhYi00ZTFmLWExNTYtZjFjNDU0NmNiZjUyIiwidG9rZW5faWQiOiIxMWEwNTk3ZS1hZGYwLTRkMjItYTM5ZS03YTA2Y2U1ODk1NzkifQ.L5OrXPbWZs8S6TWkrgfRZGTNLH3nzL87F4ouZQWdE1UlhPkQE13Pk-ybNWvfR7BI6FNcf9hAvWMqFZZ4MJXksw"
+os.environ['HATCHET_CLIENT_HOST_PORT'] = "10.1.150.105:7077"
+os.environ['HATCHET_CLIENT_TLS_STRATEGY']="none"
+
 __version__ = metadata.version("r2r")  # type: ignore
 
 
